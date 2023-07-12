5a0f2401016f3779880a2cd04cb7a73cee53e8f4
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-07-12 23:37:56 +0200

    Create app bar

---
+++ src/components/molecules/TheAppBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <nav class="app-bar">
+      <main class="app-bar__content">
+
+        <section class="app-bar__actions">
+          <button type="button" class="app-bar__action-btn">
+            <span class="material-symbols-outlined material-symbols-outlined--medium">
+              notifications
+            </span>
+          </button>  
+        </section>
+
+      </main>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+  .app-bar {
+    box-shadow: 0 7px 12px -4px rgba(#000, .1);
+    &__content {
+      padding: 0 1rem;
+      display: flex;
+      align-items: center;
+      width: 100%;
+      max-width: 1600px;
+      margin: auto;
+      height: 60px;
+    }
+    &__actions {
+      margin-left: auto;
+    }
+  }
+</style>
--- src/layouts/App.vue
+++ src/layouts/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
+    <TheAppBar />
     <router-view />
   </div>
 </template>
 
 <script setup lang="ts">
+import TheAppBar from '@/components/molecules/TheAppBar.vue'
 
 </script>
 
