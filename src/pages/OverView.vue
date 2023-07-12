0d75c57f16090f8795aa7be2a3853f5f8c7447c6
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-07-12 23:42:36 +0200

    Create add task button

--- src/pages/OverView.vue
+++ src/pages/OverView.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="l-base">
+  <div class="l-base overview">
     <main class="l-base__content">
       <h1>Overview page</h1>
     </main>
+
+    <footer>
+      <button type="button" class="overview__action-btn">
+        <span class="material-symbols-outlined material-symbols-outlined--medium">
+          add
+        </span>
+      </button>
+    </footer>
   </div>
 </template>
 
@@ -10,6 +18,20 @@
 
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .overview {
+    &__action-btn {
+      position: fixed;
+      bottom: 1rem;
+      right: 1rem;
+      width: 60px;
+      height: 60px;
+      color: #fff;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      background-color: #2D383D;
+      box-shadow: 0 7px 12px -4px rgba(#000, .5);
+    }
+  }
 </style>
