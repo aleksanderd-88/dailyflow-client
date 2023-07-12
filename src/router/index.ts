f7665fa528f2b583218b965908991fe0016ddd28
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-07-12 23:26:56 +0200

    Set page title

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import OverView from '@/pages/OverView.vue'
 
+const setPageTitle = (title: string) => {
+  document.title = `DailyFlow \u2022 ${ title }`
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'overview',
-      component: OverView
+      component: OverView,
+      beforeEnter: ((to, from) => setPageTitle('Start'))
     }
   ]
 })
