93ddab08234ce5ba4d472a241e992c907fa13eff
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-07-23 23:37:51 +0200

    Change global variable name

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,2 @@
 /// <reference types="vite/client" />
 declare const APP_VERSION: string;
-declare const window: any; //- TODO: Not a good solution, try and change in the future.
--- src/utils/version/getVersion.ts
+++ src/utils/version/getVersion.ts
@@ -1,1 +1,1 @@
-export const appVersion = () => window.__APP_VERSION__
+export const appVersion = () => APP_VERSION
