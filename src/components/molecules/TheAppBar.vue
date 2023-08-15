c3852b787e8d71033d6050aa28ce57d0383ccab7
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-08-15 09:28:09 +0200

    Implement log out button

--- src/components/molecules/TheAppBar.vue
+++ src/components/molecules/TheAppBar.vue
@@ -3,12 +3,14 @@
     <nav class="app-bar">
       <main class="app-bar__content">
 
-        <section class="app-bar__actions">
+        <section 
+          class="app-bar__actions"
+          v-if="userIsLoggedIn()"
+        >
           <button 
             type="button" 
             class="app-bar__action-btn" 
             @click.stop="bookmarkListVisible = !bookmarkListVisible"
-            v-if="userIsLoggedIn()"
           >
             <span class="material-symbols-outlined material-symbols-outlined--medium">
               notification_important
@@ -19,6 +21,14 @@
             </span>
           </button>  
 
+          <button
+            title="Log out"
+            @click="logout()"
+          >
+            <span class="material-symbols-outlined">
+              logout
+            </span>
+          </button>
         </section>
         
         <BookmarkList 
@@ -36,9 +46,18 @@ import { computed, ref } from 'vue';
 import { useBookmarkStore } from '@/stores/api/bookmark';
 import BookmarkList from './Bookmark/BookmarkList.vue';
 import { userIsLoggedIn } from '@/utils/version/authentication';
+import { useCurrentUserStore } from '@/stores/current-user';
+import { useRouter } from 'vue-router';
 
   const bookmarkListVisible = ref(false)
   const bookmarkCount = computed(() => useBookmarkStore().itemCount)
+
+  const router = useRouter()
+
+  const logout = () => {
+    useCurrentUserStore().clearCurrentUser()
+    router.replace('/')
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -56,6 +75,9 @@ import { userIsLoggedIn } from '@/utils/version/authentication';
     }
     &__actions {
       margin-left: auto;
+      display: flex;
+      gap: 1rem;
+      align-items: center;
     }
 
     &__action-btn {
